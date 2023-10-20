8effc1cb9c4ea92f3277bcde21b98adfcc8a2e7f
hevelinlima <hevelin25@gmail.com>
2023-10-20 18:55:48 -0300

    testes

--- test-exercise.py
+++ test-exercise.py
@@ -2,6 +2,11 @@ import unittest
 
 
 def str_to_bool(value):
+    try:
+        value = value.lower()
+    except:
+        raise AttributeError(f"{value} must be a string")
+
     true_values = ["y", "yes"]
     false_values = ["no", "n"]
 
@@ -19,6 +24,10 @@ class TestStrToBool(unittest.TestCase):
     def test_yes_is_true(self):
         result = str_to_bool("Yes")
         self.assertTrue(result)
+
+    def test_invalid_input(self):
+        with self.assertRaises(AttributeError):
+            str_to_bool(1)
 
 
 if __name__ == "__main__":
